8ce02602609518802d6477aca3212a46fdc813e8
jaykch <jaykch@outlook.com>
2021-04-17 23:23:48 +1000

    Added limit to tokens a user can buy and stages

--- ico.sol
+++ ico.sol
@@ -5,20 +5,26 @@ import "./token.sol";
 
 
 contract CornpopIco {
-    uint balance = 750000;
+    uint256 public balance = 750000;
+    uint public mal = 0;
     address owner;
     mapping(address => bool) whitelist;
+    mapping(address => uint) accountBalances;
     uint256 public whitelistedNumber = 0;
-    uint STAGE0 = 750000;
-    uint STAGE1 = 500000;
-    uint STAGE2 = 250000;
-    uint STAGE3 = 100000;
-    uint currentStage;
+    uint256 STAGE0 = 750000;
+    uint256 STAGE1 = 500000;
+    uint256 STAGE2 = 250000;
+    uint256 STAGE3 = 100000;
+    uint256 STAGE4 = 0;
+    uint256 public currentStage;
+    uint256 public newStage = 0;
+    uint256 public extraAmount;
+    uint256 topAmount = 10;
 
     function CornPopICO(address _tokenAddr) public {
         token = CornPop(_tokenAddr);
         owner = msg.sender;
-        currentStage = STAGE0;
+        currentStage = STAGE1;
     }
 
     modifier _ownerOnly(){
@@ -30,10 +36,12 @@ contract CornpopIco {
     CornPop token;
 
     uint256 public RATE = 1000; // 1 ETH = 1000 CPOP
+    uint256 public remainingStage = 1;
 
     function whitelistAddress(address _add) _ownerOnly public payable {
         require(!whitelist[_add], "Candidate must not be whitelisted.");
         whitelist[_add] = true;
+        accountBalances[_add] = 0;
         whitelistedNumber++;
 
     }
@@ -55,15 +63,15 @@ contract CornpopIco {
 
         if (balance <= STAGE1) {
             RATE = 750;
-            currentStage = STAGE1;
+            currentStage = STAGE2;
         }
         else if (balance <= STAGE2) {
             RATE = 500;
-            currentStage = STAGE2;
+            currentStage = STAGE3;
         }
         else if (balance <= STAGE3) {
             RATE = 250;
-            currentStage = STAGE3;
+            currentStage = STAGE4;
         }
     }
 
@@ -72,30 +80,41 @@ contract CornpopIco {
         return _weiAmount.div(10 ** 18).mul(RATE);
     }
 
+    function check(address cheking, uint256 amount) public returns (bool){
+        if (topAmount.sub(accountBalances[cheking] + amount) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     function() public payable {
 
         if (whitelist[msg.sender]) {
             uint256 weiAmnt = msg.value;
             uint256 _amount = _getTokenAmount(weiAmnt);
+
             if (balance - _amount > 0) {
-                if (!checkStage(_amount)) {
-                    balance = balance - _amount;
-                    token.transfer(msg.sender, _amount);
+                if (check(msg.sender, _amount)) {
+                    if (!checkStage(_amount)) {
+                        balance = balance - _amount;
+                        accountBalances[msg.sender] += _amount;
+                        token.transfer(msg.sender, _amount);
 
-                } else {
+                    } else {
 
-                    uint256 remainingStage = balance - currentStage;
-                    uint256 newStage = weiAmnt - (remainingStage / RATE);
-                    setRate();
-                    uint256 _newAmount = _getTokenAmount(newStage) + remainingStage;
-                    balance = balance - _newAmount;
-                    token.transfer(msg.sender, _newAmount);
-
-
+                        remainingStage = balance - currentStage;
+                        balance = balance - remainingStage;
+                        newStage = weiAmnt.div(10 ** 18) - (remainingStage / RATE);
+                        setRate();
+                        extraAmount = newStage * RATE;
+                        uint256 _newAmount = extraAmount + remainingStage;
+                        balance = balance - extraAmount;
+                        accountBalances[msg.sender] += _newAmount;
+                        token.transfer(msg.sender, _newAmount);
+                    }
                 }
-
             }
         }
-
     }
 }
